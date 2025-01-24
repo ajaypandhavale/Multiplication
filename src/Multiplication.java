1f84da9543063a73503b02adbf53068acbbf5266
ajaypandhavale <aj40135821@wipro.com>
2025-01-24 13:00:11 +0530

    creating multiplication code

---
+++ src/Multiplication.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("enter two numbers to perform multiplication ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		int mul=a*b;
+		System.out.println(" Multiplication of given numbers is "+mul);
+		
+		System.out.println(" HAVE FUN ");
+		
+
+	}
+
+}
